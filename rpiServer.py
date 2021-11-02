2827f33c36a5fea4e553d19ef1c5acb543ba97ba
Haymount <mail@mathiashojbjerg.dk>
2021-11-02 10:09:26 +0100

    motorctrl not working

--- rpiServer.py
+++ rpiServer.py
@@ -4,6 +4,7 @@ import socket
 from typing import Match
 import RPi.GPIO as g
 
+
 print("Kører serveren\n")
 
 host = "192.168.1.249" # Dette er IP-adressen for Raspberry Pi
@@ -15,41 +16,56 @@ skt.bind((host, port))
 skt.listen(1) # Lytter til indkomne forbindelser, en ad gangen
 
 g.setmode(g.BCM)
-g.setup(16, g.OUT)
-g.setup(20, g.OUT)
-g.setup(12, g.OUT)
+g.setup(16, g.OUT) #m1
+g.setup(20, g.OUT) #m2
+g.setup(12, g.OUT) #en1
+g.setup(26, g.OUT) #m3
+g.setup(21, g.OUT) #m4
+g.setup(13, g.OUT) #en2
 
-p = g.PWM(16, 50)
-
-p.start(18)
+m1 = g.PWM(16, 75)
+m3 = g.PWM(26, 75)
+#m1.start(0)
+#m3.start(0)
 
 g.output(20, 1)
-
 g.output(12, 1)
 
+g.output(13, 1)
+g.output(21, 1)
+
 def motorctrl(retning):
-    match retning:
-        case '18':
-            return 1
-        case '13':
-            return 2
-        case '14':        
-            return 0
-        case '15':
+
+
+    if retning == 18:
+        m1.ChangeDutyCycle(100)
+        m3.ChangeDutyCycle(100)
+    
+    else:
+        m1.ChangeDutyCycle(0)
+        m3.ChangeDutyCycle(0)
+
 
 
 
 while True:
+    
+    
+    m1.ChangeDutyCycle(0)
+    m3.ChangeDutyCycle(0)
+    
     forbindelse, addresse = skt.accept()
     print("Værten med " + str(addresse[0]) + " har etableret forbindelse.")
     
     while True:
         
+
         data = forbindelse.recv(64)
         decdata = data.decode("UTF-8")
         
         if data:
             print("Data: ", decdata)
+            motorctrl(decdata)
 
 
         else:
@@ -57,6 +73,6 @@ while True:
             forbindelse.close()
             break
     
-    motorctrl(decdata)
+        
 
 
