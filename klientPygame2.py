bc11d585f040712c8dd956abf73a6eb1a5d0b151
wcdk200 <sebguld@gmail.com>
2021-11-18 14:59:53 +0100

    nu med næsten batLiv

--- klientPygame2.py
+++ klientPygame2.py
@@ -7,9 +7,16 @@ angle = 0 #viklen som billedet drejer med
 
 #Det er længden og højden på winduet som popper op når programet køre
 WIDTH, HEIGHT = 900, 743
+#Her er et par faver til batteri baren
+farve1 = (219, 112, 147) #palevioletred
+farve2 = (255, 255, 0) #gul
+farve3 = (0, 0, 0) #sort
+farve4 = (0, 255, 128) #gørn ca.
+
+batLiv = 5.2 #hvor meget strøm der er
 
 pygame.init()
-WIN = pygame.display.set_mode((WIDTH, HEIGHT)) 
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #Her tegner vi selve skærmen 
 pygame.display.set_caption("Motorstyring") #Det er navnet på programet
 
 #siger lidt sig selv, men det er baggrundsfarven på vores windue(farven står i RGB)
@@ -25,11 +32,17 @@ host = "192.168.1.249" #Ip-addressen for Raspberry Pi
 port = 4200 #og ja det her er porten
 
 skt.connect((host, port))
-#
+
+#print("Værten med " + str(skt.accept[0]) + " har etableret forbindelse.")
+#encodBatLiv = skt.accept.recv(64)
+#batLiv = encodBatLiv.decode("UTF-8")
+
 def draw_windue(styr): #Her er noget styring til skærmen
     rotedede = pygame.transform.rotate(tar, angle) #Det her får billedet til at roterer
     WIN.fill(skærmfarve) #Det her er baggrundsfarven
     WIN.blit(rotedede, (styr.x, styr.y)) #Bare et billede af et rat 
+    pygame.draw.rect(WIN, farve2, (-315, 723, 500, 20)) #Her tegner vi barrti lingen (ved sku ik om den virker endnu)
+    pygame.draw.rect(WIN, farve3, (-315, 723, (batLiv*61), 20))
     pygame.display.update()
 
 def main(): #Det vigtige kode er her
@@ -47,40 +60,41 @@ def main(): #Det vigtige kode er her
                 global angle
 
                 if keys[pygame.K_w] and keys[pygame.K_d]: 
-                    data = "1,100,60"
+                    data = "1,100,60,"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
                     angle += 35
                 elif keys[pygame.K_w] and keys[pygame.K_a]:
-                    data = "1,80,100"
+                    data = "1,60,100,"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
                     angle -= 35
                     #
                 elif keys[pygame.K_w]: #Fuldskrue frem ad
-                    data = "1,100,75" #V sendes først også H muligvis den anden vej rundt
+                    data = "1,80,100," #V sendes først også H muligvis den anden vej rundt
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
+                    angle = 0
 
                 elif keys[pygame.K_a]: #Her burde den dreje til venstre
-                    data = "1,0,100"
+                    data = "1,0,100,"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
                     angle += 50
                     
                 elif keys[pygame.K_s]: #Fuldstop ind til videre -tror jeg
-                    data = "0,100,55"
+                    data = "0,100,55,"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
 
                 elif keys[pygame.K_d]: #Her dreje til højre man gør
-                    data = "1,100,0"
+                    data = "1,100,0,"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
                     angle -= 50
 
                 else:
-                    data = "0,0,0" #Her stopper bilen hvis vi ikke bruger nogle knapper
+                    data = "0,0,0," #Her stopper bilen hvis vi ikke bruger nogle knapper
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
 
