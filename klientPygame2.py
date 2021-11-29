f16a00ad149435ec99d4b0cc4d502ebae6c97362
wcdk200 <sebguld@gmail.com>
2021-11-29 10:43:14 +0100

    m

--- klientPygame2.py
+++ klientPygame2.py
@@ -5,49 +5,51 @@ import socket
 #DEn her kode har grafisk
 
 tar = pygame.image.load("Opgave/test socket/sjovTing.png") #Sejt sådan impoterer man et billede
+programIcon = pygame.image.load("Tema 2\Tema-2-Fjernstyret-bil/mette.jpg") #impoter billede til icon
 angle = 0 #viklen som billedet drejer med
 
-#Det er længden og højden på winduet som popper op når programet køre
-WIDTH, HEIGHT = 900, 743
-#Her er et par faver til batteri baren
+WIDTH, HEIGHT = 900, 743 #Det er længden og højden på winduet som popper op når programet køre
+#Her kommmer der et par farver
 farve1 = (219, 112, 147) #palevioletred
 farve2 = (255, 255, 0) #gul
 farve3 = (0, 0, 0) #sort
 farve4 = (0, 255, 128) #gørn ca.
+skærmfarve = (0, 255, 185) #siger lidt sig selv, men det er baggrundsfarven på vores windue(farven står i RGB)
+
+FPS = 60 #bestemmer maks fps. bare så det ikke ender med at lagge noget ud.
 
 batLiv = 5.2 #hvor meget strøm der er
+bbatLiv = 0
 
 pygame.init()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #Her tegner vi selve skærmen 
 pygame.display.set_caption("HasselHoff") #Det er navnet på programet
 
-#siger lidt sig selv, men det er baggrundsfarven på vores windue(farven står i RGB)
-skærmfarve = (0, 255, 185)
-
-print("Kører klienten\n")
-
-FPS = 60 #bestemmer maks fps. bare så det ikke ender med at lagge noget ud.
+print("Kører klienten\n")#Bare for at teste om programmet køre
 
 skt = socket.socket() #Forbinder til Rasp... Husk at tjekke om ip'en og porten er den rigtige
-#skt.setblocking(False)
+skt.settimeout(0.1)
 
 host = "192.168.1.249" #Ip-addressen for Raspberry Pi
 port = 4200 #og ja det her er porten
+skt.connect((host, port)) #connecter til serveren med den ip og port oven over
 
-skt.connect((host, port))
-programIcon = pygame.image.load("Tema 2\Tema-2-Fjernstyret-bil/mette.jpg")
-pygame.display.set_icon(programIcon)
-
-def modbesked(): #Det her lort virker ikke
+def modbesked(): #Det virker næsten, batLiv skal lige dif
     try:
         global batLiv
+        global bbatLiv
         mod = skt.recv(64)
-        batLiv = mod.decode("utf-8")
-        print(batLiv)
-    except:
-        print("hej")
+        bbbatLiv = mod.decode("UTF-8")
+        bbatLiv = float(bbbatLiv)
+        print(bbatLiv)
+        print("Received" + repr(bbatLiv))
+        batLiv = bbatLiv
+    except socket.error: #Burde printe det, hvis noget fucker det op oven over
+        print("fuck")
 
-def draw_windue(styr): #Her er noget styring til skærmen
+def draw_windue(styr): #Alt det grafiske og navnet på porgamet
+    pygame.display.set_icon(programIcon) #ændre det lille icon i hjørnet
+    pygame.display.set_caption("HasselHoff") #Det er navnet på programet
     rotedede = pygame.transform.rotate(tar, angle) #Det her får billedet til at roterer
     WIN.fill(skærmfarve) #Det her er baggrundsfarven
     WIN.blit(rotedede, (styr.x, styr.y)) #Bare et billede af et rat 
@@ -121,6 +123,11 @@ def main(): #Det vigtige kode er her
                     skt.sendall(nyt_data)
                     angle = 0
                     print("stop")
+                    modbesked()
+                    batLiv = int(bbatLiv)
+                    print("BatLiv " + str(batLiv))
+                    print("bb " + str(bbatLiv))
+        
         draw_windue(styr)
     pygame.quit()
 
